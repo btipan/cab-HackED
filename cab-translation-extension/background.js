47dced54f7712407ea8acb158f31b23d2f0a3b55
rpr34 <146686462+rpr34@users.noreply.github.com>
2026-02-20 14:39:18 -0700

    Create background.js

---
+++ cab-translation-extension/background.js
@@ -0,0 +1,6 @@
+/* This is the controller for:
+	•	keyboard shortcut (Ctrl/Cmd+Shift+Y)
+	•	talking to the current tab
+	•	opening/focusing your UI (popup won’t open programmatically like a panel)
+	•	passing selected text around
+*/
